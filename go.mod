7c8aab0e5bed884f2563babf81417e73b46a5d03
Andrew Shoemaker <andrew.shoemaker@gmail.com>
2023-11-05 11:06:35 -0600

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/codedByShoe/picme
+
+go 1.21.1
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1> Welcome to my awesome site</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href='mailto:andrew.shoemaker9@gmail.com'>Andrew Shoemaker</a></p>")
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+
+	}
+}
+
+func main() {
+	var router Router
+	fmt.Println("Starting the server on port 3000...")
+	err := http.ListenAndServe(":3000", router)
+	if err != nil {
+		panic(err)
+	}
+}
