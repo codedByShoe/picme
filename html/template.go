49604aa8a2b89749c6d85013ab013dde32c7ade4
Andrew Shoemaker <andrew.shoemaker@gmail.com>
2023-11-06 12:13:54 -0600

    added support for parsing embedded templates

--- html/template.go
+++ html/template.go
@@ -3,12 +3,30 @@ package html
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
 type Template struct {
 	htmlTpl *template.Template
+}
+
+func ParseFS(fs fs.FS, pattern string) (Template, error) {
+	tpl, err := template.ParseFS(fs, pattern)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{
+		htmlTpl: tpl,
+	}, nil
+}
+
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
 
 func Parse(filepath string) (Template, error) {
--- main.go
+++ main.go
@@ -3,33 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/codedByShoe/picme/actions"
 	"github.com/codedByShoe/picme/html"
+	"github.com/codedByShoe/picme/templates"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
-	// Parse templates
-	tpl, err := html.Parse(filepath.Join("templates", "index.html"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/", actions.StaticHandler(tpl))
 
-	tpl, err = html.Parse(filepath.Join("templates", "contact.html"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/contact", actions.StaticHandler(tpl))
+	r.Get("/", actions.StaticHandler(html.Must(html.ParseFS(templates.FS, "index.html"))))
+
+	r.Get("/contact", actions.StaticHandler(html.Must(html.ParseFS(templates.FS, "contact.html"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
 	fmt.Println("Starting the server on port 3000...")
-	err = http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		panic(err)
 	}
