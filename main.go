1da78d79780427a133777f1bf568fb1217e63242
Andrew Shoemaker <andrew.shoemaker@gmail.com>
2023-11-07 21:30:46 -0600

    started signup func

--- main.go
+++ main.go
@@ -4,22 +4,27 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/codedByShoe/picme/actions"
-	"github.com/codedByShoe/picme/html"
+	"github.com/codedByShoe/picme/controllers"
 	"github.com/codedByShoe/picme/templates"
+	"github.com/codedByShoe/picme/views"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", actions.StaticHandler(html.Must(html.ParseFS(templates.FS, "index.html"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "layout.html", "index.html", "partials.html"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "layout.html", "contact.html", "partials.html"))))
 
-	r.Get("/contact", actions.StaticHandler(html.Must(html.ParseFS(templates.FS, "contact.html"))))
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "layout.html", "signup.html", "partials.html"))
+	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
+
 	fmt.Println("Starting the server on port 3000...")
 	err := http.ListenAndServe(":3000", r)
 	if err != nil {
